37221bb959d30c62c8f1f1fd0a2e14b4a07adc54
Conlan2001 <conlan.m.roberts@gmail.com>
2024-02-27 14:26:33 +0000

    Added timer

---
+++ CombinedLabyrinth/Assets/MazeGenerator/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField]float remainingTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0){
+            remainingTime -= Time.deltaTime;
+        }
+
+        if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            TimerEnd();
+        }
+        
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void TimerEnd()
+    {
+        //Add what to do after timer is finsihed
+    }
+}
