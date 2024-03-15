14a0ad44b3711f3b1a62b39315707453c8bbe017
Conlan2001 <conlan.m.roberts@gmail.com>
2024-03-15 14:18:57 +0000

    Fixed cursor on gameover screen

--- CombinedLabyrinth/Assets/GameOverScreen.cs
+++ CombinedLabyrinth/Assets/GameOverScreen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
@@ -8,7 +9,18 @@ public class GameOverScreen : MonoBehaviour
     GameObject player;
     public void Setup() {
         gameObject.SetActive(true);
+        
+        //Deactivate player
         player = GameObject.FindGameObjectWithTag("Player");
         player.SetActive(false);
+
+        //Enable curson
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
