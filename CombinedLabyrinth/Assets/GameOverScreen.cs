500966c4e79a4161951f8996c9a3fadb8068881d
Conlan2001 <conlan.m.roberts@gmail.com>
2024-03-15 15:38:54 +0000

    Fixed coins appearing on game over screen

--- CombinedLabyrinth/Assets/GameOverScreen.cs
+++ CombinedLabyrinth/Assets/GameOverScreen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,6 +8,7 @@ using UnityEngine.UI;
 public class GameOverScreen : MonoBehaviour
 {
     GameObject player;
+    public TextMeshProUGUI coinText;
     public void Setup() {
         gameObject.SetActive(true);
         
@@ -17,6 +19,9 @@ public class GameOverScreen : MonoBehaviour
         //Enable curson
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+
+        coinText.text = "COINS COLLECTED: " + CoinTracker.getCoinCount();
+        CoinTracker.setCointCount(0);
     }
 
     public void MenuButton()
