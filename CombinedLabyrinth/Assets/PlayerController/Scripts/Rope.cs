a7b72035e148b7e9e9c10719eed49402509d9806
Placid_Bread <elliot.harris03@gmail.com>
2024-03-21 14:41:55 +0000

    Remove spike and coin collisions with rope

--- CombinedLabyrinth/Assets/PlayerController/Scripts/Rope.cs
+++ CombinedLabyrinth/Assets/PlayerController/Scripts/Rope.cs
@@ -132,6 +132,7 @@ public class Rope : MonoBehaviour
         if (Physics.Linecast(player.position, rope.GetPosition(_ropePositions.Count - 2), out hit, collMask))
         {
             if (ContainsSimilar(hit.point)) return;
+            if (hit.collider.CompareTag("Spike") || hit.collider.CompareTag("Coin")) return;
             _ropePositions.RemoveAt(_ropePositions.Count - 1); // remove player pos temporarily
             AddPosToRope(hit.point);
         }
