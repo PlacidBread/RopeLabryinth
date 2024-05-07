fb9a8329e562dfea35afbcab1a42ce5f30e82c5d
Placid_Bread <elliot.harris03@gmail.com>
2024-05-07 20:33:34 +0100

    Initialize with random light colours for non-realism

--- CombinedLabyrinth/Assets/MazeGenerator/Scripts/Torch.cs
+++ CombinedLabyrinth/Assets/MazeGenerator/Scripts/Torch.cs
@@ -1,18 +1,34 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MazeGenerator.Scripts
 {
     public class Torch : MonoBehaviour
     {
-        public GameObject torchLightParicles;
+        public GameObject torchLightParticles;
+        public ParticleSystem flames;
+        public ParticleSystem secondaryFlames;
+        public ParticleSystem lights;
         private GameObject player;
+
+        private void Start()
+        {
+            var colour = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+            var flamesMain = flames.main;
+            var secondaryFlamesMain = secondaryFlames.main;
+            var lightsMain = lights.main;
+            
+            flamesMain.startColor = colour;
+            secondaryFlamesMain.startColor = colour;
+            lightsMain.startColor = colour;
+        }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                torchLightParicles.SetActive(true);
+                torchLightParticles.SetActive(true);
             }
         }
     }
