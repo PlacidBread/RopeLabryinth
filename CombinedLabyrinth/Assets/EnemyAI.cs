43f55a27ea146ba66071f0b4bc640fe021a6fcf1
Conlan2001 <conlan.m.roberts@gmail.com>
2024-05-09 14:35:44 +0100

    Added ghost scenes

--- CombinedLabyrinth/Assets/EnemyAI.cs
+++ CombinedLabyrinth/Assets/EnemyAI.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
-using Random = UnityEngine.Random;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -13,7 +9,9 @@ public class EnemyAI : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
-    //Patrolling
+    public float health;
+
+    //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
@@ -24,58 +22,62 @@ public class EnemyAI : MonoBehaviour
 
     //States
     public float sightRange, attackRange;
-    public bool playerInSight, playerInAttack;
+    public bool playerInSightRange, playerInAttackRange;
 
     private void Awake()
     {
-        player = GameObject.Find("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        playerInSight = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if(!playerInSight) Patrolling();
-        else ChasePlayer();
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
 
-        OnDrawGizmosSelected();
     }
 
-    private void Patrolling()
+    private void Patroling()
     {
-        if(!walkPointSet) SearchWalkPoint();
-        if(walkPointSet) agent.SetDestination(walkPoint);
+        Debug.Log("PATROL");
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        if(distanceToWalkPoint.magnitude < 1f)
-        {
-            walkPointSet = false;  
-        }
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
     }
-
     private void SearchWalkPoint()
     {
+        //Calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y + transform.position.z + randomZ);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-        {
             walkPointSet = true;
-        }
     }
 
     private void ChasePlayer()
     {
+        Debug.Log("PATROL");
         agent.SetDestination(player.position);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
